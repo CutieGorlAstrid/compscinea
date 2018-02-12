b3576cba2824129150e1b3f3c5ddf68aea3c40d2
Arsalan Kazmi <sonicspeed848@gmail.com>
2018-02-12 10:20:19 +0000

    Delete choose.py

--- choose.py
+++
@@ -1,9 +0,0 @@
-from webbrowser import open
-print("Win or fail?")
-prompt = input("> ")
-if prompt == "win":
-    open('win.jpg')
-elif prompt == "fail":
-    open('fail.jpg')
-while True:
-    print("\n")
